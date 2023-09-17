8caa5237340abb3cae4c863b2b81720479868d83
czg <czgsci@163.com>
2023-09-17 09:11:12 +0800

    allow concurrent jobs

--- projects/concurrency-pzip/pzip.c
+++ projects/concurrency-pzip/pzip.c
@@ -1,3 +1,9 @@
+/*
+$ rm test.txt;pagesize=4096;offset=596;for i in $(seq ${pagesize});do echo -n 'a'>>test.txt;done;echo -n 'aaa'>>test.txt;for i in $(seq ${offset});do echo -n 'b'>>test.txt;done
+$ ./pzip test.txt 
+connect between pages with a
+4099a596b%                                   
+*/
 #include "thread_helper.h"
 #include <arpa/inet.h> // htonl
 #include <fcntl.h>     // open, O_* constants
@@ -9,13 +15,32 @@
 #include <sys/types.h>
 #include <unistd.h> // sysconf, close
 
+// #define DEBUG_ECHO
+#define ECHO_NUM
+#define DEBUG_PROCESS
+/*
+here `ALLOW_MULTIPLE_JOBS` only allow multiple pending jobs but not concurrent because they are all in the main thread.
+*/
+#define ALLOW_MULTIPLE_JOBS
+#define ALLOW_MULTIPLE_JOBS_DEBUG
+
+/*
 // http://www.catb.org/esr/structure-packing/
+based on the largest size TODO read after 6. Bitfields
+See
+> Because s only needs to be 2-byte aligned
+*/
+/*
 // https://en.wikipedia.org/wiki/Data_structure_alignment
+1. always from big to small size See "MixedData After compilation in 32-bit x86 machine"
+> As long as the memory word size is at least as large as the largest primitive data type supported by the computer,
+2. https://en.wikipedia.org/wiki/Data_structure_alignment#Typical_alignment_of_C_structs_on_x86
+*/
 typedef struct result {
   struct result *next;
   int count;
   char character;
-  char pad[sizeof(struct result *) - sizeof(int) - 1];
+  char pad[sizeof(struct result *) - sizeof(int) - 1]; // align to 8 bytes
 } Result;
 
 typedef struct work {
@@ -26,8 +51,8 @@ typedef struct work {
 
 typedef struct files {
   int fd;
-  char pad[sizeof(off_t) - sizeof(int)];
   off_t size;
+  char pad[sizeof(off_t) - sizeof(int)];
 } Files;
 
 static long long use_ptr = 0, fill_ptr = 0, chunks = 0;
@@ -56,9 +81,16 @@ static void *compress(void *arg) {
 
     // get work
     Work *current_work = &works[use_ptr];
+    #ifdef ALLOW_MULTIPLE_JOBS_DEBUG
+    printf("use_ptr: %lld\n",use_ptr);
+    #endif
     use_ptr = (use_ptr + 1) % chunks;
 
     // do work
+    /*
+    different from wzip, Result is all written at the end instead of written one by one.
+    TODO the count is separated by page unit, needing condensed at the last
+    */
     Result *head = NULL;
     Result *previous_result = NULL;
     char previous_character = '\0';
@@ -74,6 +106,9 @@ static void *compress(void *arg) {
           if (previous_result != NULL)
             previous_result->next = last_result;
           previous_result = last_result;
+          /*
+          only assign once.
+          */
           if (head == NULL)
             head = previous_result;
         }
@@ -86,24 +121,47 @@ static void *compress(void *arg) {
       current_work->results = create_result(previous_count, previous_character);
     } else {
       current_work->results = head;
+      /*
+      This either store the one character by `previous_count=1`
+      or `previous_count>1` count `previous_character`.
+      */
       previous_result->next = create_result(previous_count, previous_character);
     }
 
     Sem_post(&mutex);
+    #ifdef ALLOW_MULTIPLE_JOBS
+    #else
     Sem_post(&empty);
+    #endif
   }
 }
 
 // Littleendian and Bigendian byte order illustrated
 // https://dflund.se/~pi/endian.html
 static void writeFile(int character_count, char *oldBuff) {
-  character_count = htonl(character_count); // write as network byte order
+  // character_count = htonl(character_count); // write as network byte order
+  /*
+  same as wzip
+  > a 4-byte integer followed by a character for each run
+  */
+  #ifdef DEBUG_ECHO
+  int test=6;
+  // fwrite(&test, sizeof(int), 1, stdout);
+  fprintf(stdout, "%d", test);
+  #endif
+  #ifdef ECHO_NUM
+  fprintf(stdout, "%d", character_count);
+  #else
   fwrite(&character_count, sizeof(int), 1, stdout);
+  #endif
   fwrite(oldBuff, sizeof(char), 1, stdout);
 }
 
 int main(int argc, char *argv[]) {
   long page_size = sysconf(_SC_PAGE_SIZE);
+  #ifdef DEBUG_ECHO
+  printf("pagesize:%ld\n",page_size);
+  #endif
 
   if (argc <= 1) {
     fprintf(stdout, "pzip: file1 [file2 ...]\n");
@@ -111,6 +169,9 @@ int main(int argc, char *argv[]) {
   }
 
   // get_nprocs is GNU extension
+  /*
+  note 2 in README
+  */
   long np = sysconf(_SC_NPROCESSORS_ONLN);
   pthread_t *threads = malloc(sizeof(pthread_t) * (unsigned long)np);
   if (threads == NULL)
@@ -147,6 +208,10 @@ int main(int argc, char *argv[]) {
   if (works == NULL)
     handle_error("malloc");
 
+  /*
+  Here maybe each lock for each file is better.
+  the empty and full ensures the consecutive processing between compress and jobs.
+  */
   // create workers
   for (long i = 0; i < np; i++)
     Pthread_create(&threads[i], NULL, compress, works);
@@ -155,13 +220,26 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < argc - 1; i++) {
     long long offset = 0;
     while (offset < files[i].size) {
+      #ifdef ALLOW_MULTIPLE_JOBS
+      #else
       Sem_wait(&empty);
+      #endif
+      /*
+      although we can let each `works` thread use one independent `mutex` to manipulate with something like
+      `works[fill_ptr-fill_ptr%np+thread_id]`. then `jobs` threads also need to be np instead of one here.
+      */
       Sem_wait(&mutex);
 
       works[fill_ptr].chunk_size = page_size;
+      #ifdef ALLOW_MULTIPLE_JOBS_DEBUG
+      printf("job %d offset: %lld;fill_ptr:%lld\n",i,offset,fill_ptr);
+      #endif
       if (offset + page_size > files[i].size)
         works[fill_ptr].chunk_size = files[i].size - offset;
-
+      
+      /*
+      note 4 in README
+      */
       char *addr = mmap(NULL, (size_t)works[fill_ptr].chunk_size, PROT_READ,
                         MAP_PRIVATE, files[i].fd, offset);
       if (addr == MAP_FAILED)
@@ -170,6 +248,9 @@ int main(int argc, char *argv[]) {
       works[fill_ptr].addr = addr;
       works[fill_ptr].results = NULL;
       offset += page_size;
+      /*
+      TODO the % seems to be abundant by `chunks += (sb.st_size / page_size + 1);`.
+      */
       fill_ptr = (fill_ptr + 1) % chunks;
 
       Sem_post(&mutex);
@@ -179,14 +260,33 @@ int main(int argc, char *argv[]) {
   }
 
   // check jobs are done
+  /*
+  i.e. compress has finished the job by `Sem_post(&empty);`.
+  */
+  #ifdef ALLOW_MULTIPLE_JOBS
+  #else
   Sem_wait(&empty);
+  #endif
+  /*
+  Avoid to capture the posted mutex by the main thread jobs loop.
+  */
+  #ifndef ALLOW_MULTIPLE_JOBS
   Sem_wait(&mutex);
+  #endif
 
   // kill and wait workers
   for (long i = 0; i < np; i++) {
+    /*
+    https://stackoverflow.com/a/12280247/21294350
+    no signal is expected, so no `pthread_kill`
+    `Pthread_cancel` not immediately cancel because "deferred (the default for new threads)" in `man pthread_cancel`
+    */
     Pthread_cancel(threads[i]);
     Pthread_join(threads[i], NULL);
   }
+  #ifdef ALLOW_MULTIPLE_JOBS
+  Sem_wait(&mutex);
+  #endif
 
   // final compress
   int last_count = 0;
@@ -194,18 +294,39 @@ int main(int argc, char *argv[]) {
   for (long long i = 0; i < chunks; i++) {
     Result *result;
     result = works[i].results;
+    /*
+    take the very first, first, last, the last of whole special cases in account.
+    */
     while (result != NULL) {
+      /*
+      here first and last need separate manipulation because the character sequence 
+      may be splitted into different page units
+      and they are manipulated together as one whole entity.
+      */
       if (result == works[i].results &&
           result->next != NULL) { // first but not last result
         if (result->character == last_character) {
+          /*
+          connect the page boundary.
+          */
+          #ifdef DEBUG_PROCESS
+          printf("connect between pages with %c\n",result->character);
+          #endif
           writeFile(result->count + last_count, &result->character);
         } else {
+          /*
+          by `if (previous_count != 0) {` and `previous_count = 1`, last_count can't be 0
+          so it only excludes the init state `int last_count = 0`.
+          */
           if (last_count > 0)
             writeFile(last_count, &last_character);
           writeFile(result->count, &result->character);
         }
       } else if (result->next == NULL) {  // last result
         if (result != works[i].results) { // not first
+          /*
+          ensure the last chunk written at its manipulation process instead of at the next non-exist first result in the next page.
+          */
           if (i == chunks - 1) {          // last chunk
             writeFile(result->count, &result->character);
           } else { // not last chunk
@@ -213,6 +334,13 @@ int main(int argc, char *argv[]) {
             last_count = result->count;
           }
         } else {                                     // first result
+          /*
+          1. then it needs to check whether same as the last page last character
+          which is also done in above "// first but not last result".
+          1.1 if the current last and first not overlaps (i.e. they are separated by other different characters),
+          it implies not equal because the first will check
+          whether equal before the last.
+          */
           if (result->character == last_character) { // same
             if (i != chunks - 1) {                   // not last chunk
               last_count += result->count;
@@ -241,7 +369,10 @@ int main(int argc, char *argv[]) {
     if (munmap(works[i].addr, (size_t)works[i].chunk_size) != 0)
       handle_error("munmap");
   }
-  Sem_post(&mutex);
+  /*
+  This seems to be no use, because no other threads request the mutex.
+  */
+  // Sem_post(&mutex);
 
   free(threads);
   free(files);
